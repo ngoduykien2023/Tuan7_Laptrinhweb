967ddcff7a6c1694cd4c9d97df218d59dac94ba1
ngoduykien2023 <kien0937862293@gmail.com>
2024-10-24 13:53:18 +0700

    code php

---
+++ ketnoiDatabase.php
@@ -0,0 +1,14 @@
+
+<!--------------------------------------------------------------- code php --------------------------------------------------------------->
+
+<?php
+    $serverName = "localhost";
+    $userName = "root";
+    $pwd = "";
+    $nameDB = "dblaptop";
+    $conn = mysqli_connect($serverName, $userName, $pwd, $nameDB);
+
+    if ($conn == false) { 
+        echo "kết nối thất bại";
+    }
+?>
